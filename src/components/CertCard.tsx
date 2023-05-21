d5aec0cdbb04f888533401230c058f40a16a9038
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-05-21 14:18:26 -0400

    certifications fade in

--- src/components/CertCard.tsx
+++ src/components/CertCard.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 type Certification = {
 	id: number;
 	pic: string;
@@ -8,26 +6,20 @@ type Certification = {
 };
 
 const CertCard = (cert: Certification) => {
-	const [desc, setDesc] = useState(false);
-	const showDesc = () => setDesc(true);
-	const hideDesc = () => setDesc(false);
-
 	return (
 		<a
-			onMouseOver={showDesc}
-			onMouseOut={hideDesc}
+			className='cert'
 			href={cert.url}
 			key={cert.id}>
 			<div
 				style={{ position: 'relative' }}
-				className={desc ? 'certInnerDesc' : 'certInner'}>
+				className='certInner'>
 				<img
 					className='certPic'
 					src={cert.pic}
 					alt={cert.desc}
 				/>
 			</div>
-			<span className='certDesc'>{cert.desc}</span>
 		</a>
 	);
 };
--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -12,6 +12,24 @@ type Certifications = {
 };
 
 function Certifications({ certs }: Certifications) {
+	const reveal = () => {
+		const certs = document.querySelectorAll('.cert');
+		for (let i = 0; i < certs.length; i++) {
+			const windowHeight = window.innerHeight;
+			const elementTop = certs[i].getBoundingClientRect().top;
+			const elementVisible = 150;
+			if (elementTop < windowHeight - elementVisible) {
+				certs[i].classList.remove('hidden');
+				certs[i].classList.add('shown');
+			} else {
+				certs[i].classList.remove('shown');
+				certs[i].classList.add('hidden');
+			}
+		}
+	};
+	window.addEventListener('scroll', reveal);
+	reveal();
+
 	return (
 		<div className='certifications'>
 			{certs.map(cert => (
