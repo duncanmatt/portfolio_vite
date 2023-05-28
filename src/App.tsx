003f131829f22c3d3dcb9bc639f7850ff78be000
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-05-28 18:47:23 -0400

    remove skills from page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Navbar from './layout/Navbar';
 import Intro from './layout/Intro';
 import Latest from './layout/Latest';
-import Skills from './layout/Skills';
 import Certified from './layout/Certified';
 import Footer from './layout/Footer';
 
@@ -12,7 +11,6 @@ function App() {
 			<main>
 				<Intro />
 				<Latest />
-				<Skills />
 				<Certified />
 			</main>
 			<Footer />
--- src/components/SkillsIcons.tsx
+++ src/components/SkillsIcons.tsx
@@ -6,83 +6,83 @@ const SkillsIcons = () => {
 		query: '(min-width: 800px)',
 		minDeviceWidth: 800,
 		type: 'screen',
-	})
+	});
 
 	return (
-		<div className='skillIconsWrapper'>
-			<ul className={isLarge ? 'skillIconsLg' : 'skillIcons'}>
-					<li className='skillIconOuter'>
-						<img
-							className='skillIconInner'
-							src={
-								'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
-							}
-							alt='HTML5'
-						/>
-					</li>
-					<li className='skillIconOuter'>
-						<img
-							className='skillIconInner'
-							src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
-							alt='CSS3'
-						/>
-					</li>
-					<li className='skillIconOuter'>
-						<img
-							className='skillIconInner'
-							src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
-							alt='JavaScript'
-						/>
-					</li>
-					<li className='skillIconOuter'>
-						<img
-							className='skillIconInner'
-							src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg'
-							alt='Figma'
-						/>
-					</li>
-					<li className='skillIconOuter'>
-						<img
-							className='skillIconInner'
-							src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
-							alt='React'
-						/>
-					</li>
-					<li className='skillIconOuter'>
-						<img
-							className='skillIconInner'
-							src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg'
-							alt='Node.js'
-						/>
-					</li>
-					<li className='skillIconOuter'>
-						<img
-							className='skillIconInner'
-							src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg'
-							alt='TypeScript'
-						/>
-					</li>
-					<li className='skillIconOuter'>
-						<img
-							className='skillIconInner'
-							src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg'
-							alt='MongoDB'
-						/>
-					</li>
-					<li className='skillIconOuter'>
-						<img
-							className='skillIconInner'
-							src={expressWhite}
-							alt='Express.js'
-						/>
-					</li>
-					<li className='skillIconOuter'>
-						<img
-							className='skillIconInner'
-							src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg'
-							alt='Redux'
-						/>
-					</li>
+		<div className='skillItemsWrapper'>
+			<ul className={isLarge ? 'skillItemsLg' : 'skillItems'}>
+				<li className='skillItemOuter'>
+					<img
+						className='skillIcon'
+						src={
+							'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
+						}
+						alt='HTML5'
+					/>
+				</li>
+				<li className='skillItemOuter'>
+					<img
+						className='skillIcon'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
+						alt='CSS3'
+					/>
+				</li>
+				<li className='skillItemOuter'>
+					<img
+						className='skillIcon'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
+						alt='JavaScript'
+					/>
+				</li>
+				<li className='skillItemOuter'>
+					<img
+						className='skillIcon'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg'
+						alt='Figma'
+					/>
+				</li>
+				<li className='skillItemOuter'>
+					<img
+						className='skillIcon'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
+						alt='React'
+					/>
+				</li>
+				<li className='skillItemOuter'>
+					<img
+						className='skillIcon'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg'
+						alt='Node.js'
+					/>
+				</li>
+				<li className='skillItemOuter'>
+					<img
+						className='skillIcon'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg'
+						alt='TypeScript'
+					/>
+				</li>
+				<li className='skillItemOuter'>
+					<img
+						className='skillIcon'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg'
+						alt='MongoDB'
+					/>
+				</li>
+				<li className='skillItemOuter'>
+					<img
+						className='skillIcon'
+						src={expressWhite}
+						alt='Express.js'
+					/>
+				</li>
+				<li className='skillItemOuter'>
+					<img
+						className='skillIcon'
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg'
+						alt='Redux'
+					/>
+				</li>
 			</ul>
 		</div>
 	);
