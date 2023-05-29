433a1c60e718ee866adb9c48b37c3958ae82787e
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-05-28 17:33:51 -0700

    skills on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from './layout/Navbar';
 import Intro from './layout/Intro';
 import Latest from './layout/Latest';
+import Skills from './layout/Skills';
 import Certified from './layout/Certified';
 import Footer from './layout/Footer';
 
@@ -11,6 +12,7 @@ function App() {
 			<main>
 				<Intro />
 				<Latest />
+				<Skills />
 				<Certified />
 			</main>
 			<Footer />
