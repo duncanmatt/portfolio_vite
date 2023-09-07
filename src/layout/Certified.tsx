b005a4e5e4092347e7531e092bbcf9c0809b2ef8
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-09-06 22:45:38 -0400

    store certs externally

--- src/layout/Certified.tsx
+++ src/layout/Certified.tsx
@@ -1,25 +1,22 @@
-import DSAPic from '../layout/assets/dsa.jpg';
-import ReactPic from '../layout/assets/reactCert.jpg';
-import GoogleCloudPic from '../layout/assets/googleCloudPic.jpg';
 import Certifications from '../components/Certifications';
 
 function Certified() {
 	const certs = [
 		{
 			id: 1,
-			pic: DSAPic,
+			pic: 'https://d1suwrqf4r82dt.cloudfront.net/JSAlgoCert.webp',
 			desc: 'Took this course from Colt Steele after getting comfortable with JavaScript.',
 			url: 'https://www.udemy.com/certificate/UC-74c1725e-da46-415b-9268-9ac752f3145d',
 		},
 		{
 			id: 2,
-			pic: GoogleCloudPic,
+			pic: 'https://d1suwrqf4r82dt.cloudfront.net/ccpAWSCert.webp',
 			desc: 'Was inspired to learn a programming language after having fun playing around with HTML/CSS.',
 			url: 'https://www.coursera.org/account/accomplishments/verify/867P3BDJHCMP',
 		},
 		{
 			id: 3,
-			pic: ReactPic,
+			pic: 'https://d1suwrqf4r82dt.cloudfront.net/ReactFTBCert.webp',
 			desc: 'A great course from Brad Traversy, one of my favorite content creators. I would highly recommend to anyone wanting to start building modern apps.',
 			url: 'https://www.udemy.com/certificate/UC-cdf8de33-c1ea-462d-af3e-1f9f47f13676/',
 		},
