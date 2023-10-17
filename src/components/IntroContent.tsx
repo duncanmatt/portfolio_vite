ea035b0ccf1fdbc759e3ed3574d3197af2736c1c
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-10-17 14:13:38 -0400

    circle headshot and lighten skill icons

--- src/components/IntroContent.tsx
+++ src/components/IntroContent.tsx
@@ -1,27 +1,27 @@
 import SkillsIcons from './SkillsIcons';
 
 const IntroContent = () => {
-	const title = `
+  const title = `
     Hello! I'm Matt Duncan, self-taught developer from northern Virginia.
   `;
 
-	return (
-		<div className='introContent'>
-			<div className='info'>
-				<span className='briefAbout'>{title}</span>
-			</div>
-			<figure>
-				<div className='headshotWrapper'>
-					<img
-						className='headshot'
-						src='https://d1suwrqf4r82dt.cloudfront.net/mugshotBlk.webp'
-						alt='Matt Duncan'
-					/>
-				</div>
-			</figure>
-			<SkillsIcons />
-		</div>
-	);
+  return (
+    <div className='introContent'>
+      <div className='info'>
+        <span className='briefAbout'>{title}</span>
+      </div>
+      <figure>
+        <div className='headshotWrapper'>
+          <img
+            className='headshot'
+            src='https://d1suwrqf4r82dt.cloudfront.net/headShotNoBG.png'
+            alt='Matt Duncan'
+          />
+        </div>
+      </figure>
+      <SkillsIcons />
+    </div>
+  );
 };
 
 export default IntroContent;
