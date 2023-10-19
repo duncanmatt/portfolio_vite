4e0a184d579f97fd98225a0bde6fd91d3b15f744
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-10-19 13:19:30 -0400

    remove skills from layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import Navbar from './layout/Navbar';
 import Intro from './layout/Intro';
 import Latest from './layout/Latest';
-import Skills from './layout/Skills';
 import Certified from './layout/Certified';
 import Footer from './layout/Footer';
 
 function App() {
-  return (
-    <>
-      <a id='skipNav' href='#content'>
-        Skip to main content
-      </a>
-      <Navbar />
-      <main id='content'>
-        <Intro />
-        <Latest />
-        <Skills />
-        <Certified />
-      </main>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<a
+				id='skipNav'
+				href='#content'>
+				Skip to main content
+			</a>
+			<Navbar />
+			<main id='content'>
+				<Intro />
+				<Latest />
+				<Certified />
+			</main>
+			<Footer />
+		</>
+	);
 }
 
 export default App;
