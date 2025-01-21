4cf615c7cb5dba8bf99d064e2a409b7c10332b24
Matt Duncan <mmduncandev@gmail.com>
2025-01-20 21:20:24 -0500

    change theme

--- src/components/LatestContent.tsx
+++ src/components/LatestContent.tsx
@@ -9,7 +9,7 @@ import 'swiper/css/pagination';
 const items = [
 	{
 		id: 0,
-		title: 'BLACK SHEEP WORLD',
+		title: 'Black Sheep World',
 		desc: 'Ecommerce web app built with Next.js that utilizes lambda functions to efficiently perform crucial functionality.',
 		pic: 'https://d1suwrqf4r82dt.cloudfront.net/ecommPreview.webp',
 		url: 'https://dev.d2ev7e5869alah.amplifyapp.com',
@@ -18,14 +18,14 @@ const items = [
 	{
 		id: 1,
 		title: 'Dollar Detective',
-		desc: 'Web app made with Next.js which consolidates necessary information for efficient draft research.',
+		desc: 'iOS app that lets users view the latest CPI data and grocery prices in an intuitive interface',
 		pic: 'https://d1suwrqf4r82dt.cloudfront.net/ddApp.png',
 		url: 'https://apps.apple.com/us/app/dollar-detective/id6736560507',
 		urlGH: 'https://github.com/duncanmatt/DollarDetective',
 	},
 	{
 		id: 2,
-		title: 'GITHUB FINDER',
+		title: 'GitHub Search',
 		desc: 'React app that gives users the ability to search for and view profiles on GitHub by making use of the GitHub API.',
 		pic: 'https://d1suwrqf4r82dt.cloudfront.net/ghPreview.webp',
 		url: 'https://github-finder-rho-ivory.vercel.app/',
@@ -33,7 +33,7 @@ const items = [
 	},
 	{
 		id: 3,
-		title: 'HOUSE MARKETPLACE',
+		title: 'House Marketplace',
 		desc: 'React app for posting or viewing real estate listings that are stored with firebase',
 		pic: 'https://d1suwrqf4r82dt.cloudfront.net/rePreview.webp',
 		url: 'https://house-marketplace-nu-ochre.vercel.app/',
