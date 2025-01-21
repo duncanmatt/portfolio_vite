e2ee40d935a056fbab4b9438d53fcbe20c9e3a78
Matt Duncan <mmduncandev@gmail.com>
2025-01-20 21:46:02 -0500

    prebuild js in assets

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: './',
 	build: {
 		rollupOptions: {
-			external: ['main.tsx']
+			external: ['main.tsx', './assets/*.js']
 		}
 	}
 });
